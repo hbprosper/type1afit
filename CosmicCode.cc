b813b2b241b65f6345226971f05b0585f023baba
Harrison Prosper <hbprosper@gmail.com>
2021-10-16 20:24:53 -0400

    use iminuit

--- CosmicCode.cc
+++ CosmicCode.cc
@@ -24,36 +24,36 @@ struct CosmicCode
 {
   struct CModel
   {
-    int model;
+    int ID;
 
-    CModel(int _model=0) : model(_model)
+    CModel(int _id=0) : ID(_id)
     {
-      switch (model)
-	{
-	case 0: // LCDM
-	default:
-	  cout << endl << "\tLCDM model" << endl << endl;
-	  break;
-	case 1: // phantom
-	  cout << endl << "\tphantom model" << endl << endl;
-	  break;
-	}    
+      switch (ID)
+        {
+        case 0: // LCDM
+        default:
+          cout << endl << "\tLCDM model" << endl << endl;
+          break;
+        case 1: // phantom
+          cout << endl << "\tphantom model" << endl << endl;
+          break;
+        }    
     }
     ~CModel() {}
   
     double operator()(double a, double* p)
     {
-      // p[0] = OL
-      // p[1] = OM
-      // p[2] = H0
-      // p[3] = n
       assert(p);
       double y = 0;
-      switch (model)
+      switch (ID)
 	{
 	case 0: // LCDM
 	default:
 	  {
+	    // p[0]: OM
+	    // p[1]: OL
+	    // p[2]: H0
+	   
 	    // a^3 * [Omega_M/a^3 + (1-Omega_M-Omega_L)/a^2 + Omega_L]
 	    double OM = p[0];
 	    double OL = p[1];
@@ -63,8 +63,11 @@ struct CosmicCode
 	  break;
 	case 1: // phantom
 	  {
+	    // p[0]: n
+	    // p[1]: H0
+	    //
 	    // a^3 * [ exp(a^n-1)/a^3 ]
-	    double n = p[3];
+	    double n = p[0];
 	    y = exp(pow(a, n)-1);
 	  }
 	  break;
@@ -78,8 +81,13 @@ struct CosmicCode
   double offset;
   int N;
   
-  CosmicCode(int _ID, int _N=200)
-    : model(CModel(_ID)),
+  void* address()
+  {
+      return (void*)this;
+  }
+    
+  CosmicCode(int _id, int _N=200)
+    : model(CModel(_id)),
       N(_N),
       offset(5*log10(2.99*pow(10.0, 5.0)) + 25)
   {}
@@ -88,30 +96,49 @@ struct CosmicCode
   // compute distance modulus
   double distanceModulus(double z, double* p)
   {
-    double OM = p[0];
-    double OL = p[1];
-    double H0 = p[2];
+    double OM;
+    double OL;
+    double H0;
+
+    switch (model.ID)
+      {
+      case 0: // LCDM
+      default:
+	{
+	  OM = p[0];
+	  OL = p[1];
+	  H0 = p[2];
+	}
+	break;
+      case 1: // phantom
+	{
+	  OM = 1;
+	  OL = 0;
+	  H0 = p[1];
+	}
+	break;
+      }
     
     double a = 1.0/(1+z);
     double h = (1-a) / N;
     double F = 0;
     for(int i=0; i < N; i++)
       {
-	double x = a + (i+0.5)*h;
-	F = F + 1.0/sqrt(x * model(x, p));
+        double x = a + (i+0.5)*h;
+        F = F + 1.0/sqrt(x * model(x, p));
       }
     F = F*h;
 
     double OK = 1 - OM - OL;
     if ( OK != 0 )
       {
-	double rootOK = sqrt(fabs(OK));
-	double theta  = rootOK * F;
-	if ( OK > 0 )
-	  F = sinh(theta);  // sinh
-	else
-	  F = sin(theta);   // sin
-	F /= rootOK;
+        double rootOK = sqrt(fabs(OK));
+        double theta  = rootOK * F;
+        if ( OK > 0 )
+          F = sinh(theta);  // sinh
+        else
+          F = sin(theta);   // sin
+        F /= rootOK;
       }
 
     double y = 5*log10( (1+z) * F / H0) + offset;
@@ -126,10 +153,10 @@ struct CosmicCode
     double h = amax / N;
     for(int i=0; i < N; i++)
       {
-	double x = (i+0.5)*h;
-	F = F + sqrt(x / model(x, p));
-	a[i] = x + 0.5*h;
-	t[i] = F*h;
+        double x = (i+0.5)*h;
+        F = F + sqrt(x / model(x, p));
+        a[i] = x + 0.5*h;
+        t[i] = F*h;
       }
   }
 
@@ -141,10 +168,10 @@ struct CosmicCode
     double h = amax / N;
     for(int i=0; i < N; i++)
       {
-	double x = (i+0.5)*h;
-	F = F + 1.0 / sqrt(x * model(x, p));
-	a[i] = x + 0.5*h;
-	chi[i] = F*h;
+        double x = (i+0.5)*h;
+        F = F + 1.0 / sqrt(x * model(x, p));
+        a[i] = x + 0.5*h;
+        chi[i] = F*h;
       }
   }
 
@@ -154,9 +181,9 @@ struct CosmicCode
     double h = amax / N;
     for(int i=0; i < N; i++)
       {
-	double x = (i+0.5)*h;
-	a[i] = x + 0.5*h;
-	O[i] = model(a[i], p) / pow(a[i], 3);
+        double x = (i+0.5)*h;
+        a[i] = x + 0.5*h;
+        O[i] = model(a[i], p) / pow(a[i], 3);
       }
   }
 
